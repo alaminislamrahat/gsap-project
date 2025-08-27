933171d207a378b54a990cbe12e5782a277bed9d
alaminislamrahat <alaminislamrahat@gmail.com>
2025-08-28 00:20:13 +0600

    hero animation is finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import NavBar from './Components/NavBar'
 import Hero from './Sections/Hero'
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <NavBar/>
       <Hero/>
+      <div className='h-dvh border border-red-500'></div>
     </main>
   )
 }
