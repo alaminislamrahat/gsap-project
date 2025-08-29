81d24432736194888b9cdf61b52aaac975fe76e2
alaminislamrahat <alaminislamrahat@gmail.com>
2025-08-29 18:49:10 +0600

    smooth scrolling added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
 import NavBar from './Components/NavBar'
 import Hero from './Sections/Hero'
 import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/all';
+import { ScrollSmoother, ScrollTrigger } from 'gsap/all';
 import MessageSection from './Sections/MessageSection';
 import FlavourSection from './Sections/FlavourSection';
+import { useGSAP } from '@gsap/react';
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App = () => {
+
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true
+    })
+  })
+
   return (
     <main>
-      <NavBar/>
-      <Hero/>
-      <MessageSection/>
-      <FlavourSection/>
-      <div className='h-dvh border border-red-500'></div>
+      <NavBar />
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <Hero />
+          <MessageSection />
+          <FlavourSection />
+          <div className='h-dvh border border-red-500'></div>
+        </div>
+      </div>
+
     </main>
   )
 }
