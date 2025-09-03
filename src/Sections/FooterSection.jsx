c4804f3dcf7b5d2c9369d76a1e67b4122dbf5575
alaminislamrahat <alaminislamrahat@gmail.com>
2025-09-04 00:45:38 +0600

    footer work initially done

--- src/Sections/FooterSection.jsx
+++ src/Sections/FooterSection.jsx
@@ -26,6 +26,29 @@ const FooterSection = () => {
           </div>
        
         </div>
+
+        <div className="mt-40 md:px-10 px-5 flex gap-10 md:flex-row flex-col justify-between text-milk font-paragraph md:text-lg font-medium">
+          <div className="flex items-center md:gap-16 gap-5">
+            <div>
+              <p>SPYLT Flavors</p>
+            </div>
+
+            <div>
+              <p>Chung Clab</p>
+              <p>Student Marketing</p>
+              <p>Dairy Dealers</p>
+            </div>
+            <div>
+              <p>Company</p>
+              <p>Contacts</p>
+              <p>Tasty Talk</p>
+            </div>
+          </div>
+
+          <div className="max-w-lg">
+            <p>Get Exclusive Early Access and Stay Informed About Product</p>
+          </div>
+        </div>
       </div>
     </section>
   )
